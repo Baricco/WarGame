0cb80e4e18fc221d23085376a26ba8aa93ca69fc
Baricco <filibarucci@gmail.com>
2024-03-15 16:58:30 +0100

    oggi mi uccido

--- src/main/java/com/example/mainPageController.java
+++ src/main/java/com/example/mainPageController.java
@@ -1,12 +1,24 @@
 package com.example;
 
 import java.io.IOException;
+import java.io.InputStream;
+
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
 
 public class mainPageController {
+
+
+    private void initWindow() {
+        
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
         System.out.println("Hai premuto il pulsantozzo");
     }
+
 }

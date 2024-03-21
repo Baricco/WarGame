2807501fdc1e53c01aa7a50f420b9276453e4fe9
LorenzoSomma <317154@studenti.unimore.it>
2024-03-21 15:50:07 +0100

    io mi chiamo tony ripeti tre volte il nome tony tony tony

---
+++ src/main/java/com/assets/SVGAssets/SVGParser.java
@@ -0,0 +1,16 @@
+package com.assets.SVGAssets;
+
+import java.util.ArrayList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SVGParser {
+    String fileName;
+    ArrayList<String> paths;
+    public SVGParser(String fileName) {
+        this.fileName = fileName;
+
+    }
+}

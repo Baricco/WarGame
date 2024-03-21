9426f5f78e3ffc427231dc1262dcb4c3ccdc29a6
Baricco <filibarucci@gmail.com>
2024-03-21 16:59:22 +0100

    ho finito il parser

--- src/main/java/com/assets/SVGAssets/SVGParser.java
+++ src/main/java/com/assets/SVGAssets/SVGParser.java
@@ -1,16 +1,30 @@
 package com.assets.SVGAssets;
 
+import java.io.File;
 import java.util.ArrayList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class SVGParser {
-    String fileName;
-    ArrayList<String> paths;
+    
+    private Document file;
+
+    
     public SVGParser(String fileName) {
-        this.fileName = fileName;
+        try { this.file = Jsoup.parse(new File(fileName)); } catch(Exception e) { System.out.println("Error, File doesn't exist"); }
+    }
+
+    public ArrayList<String> parseFile() { 
+        
+        ArrayList<Element> paths = new ArrayList<Element>();
+        ArrayList<String> stringPaths = new ArrayList<String>();
+
+        System.out.println(paths.addAll(this.file.body().getElementsByTag("path")));
+
+        for (Element path : paths) stringPaths.add(path.attribute("d").toString().substring(3));
+
+        return stringPaths;
 
     }
 }

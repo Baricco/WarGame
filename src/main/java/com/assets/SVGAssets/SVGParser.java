4b6207797b1d4314b9738514a44c85983d02521f
Baricco <filibarucci@gmail.com>
2024-03-22 15:31:01 +0100

    ho provato a risolvere ma non sono andato molto avanti

--- src/main/java/com/assets/SVGAssets/SVGParser.java
+++ src/main/java/com/assets/SVGAssets/SVGParser.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.scene.shape.SVGPath;
+import javafx.util.Pair;
 
 public class SVGParser {
     
@@ -15,16 +21,46 @@ public class SVGParser {
         try { this.file = Jsoup.parse(new File(fileName)); } catch(Exception e) { System.out.println("Error, File doesn't exist"); }
     }
 
-    public ArrayList<String> parseFile() { 
-        
-        ArrayList<Element> paths = new ArrayList<Element>();
-        ArrayList<String> stringPaths = new ArrayList<String>();
-        paths.addAll(this.file.body().getElementsByTag("path"));
+    public ArrayList<Pair<String, String>> parseFile() { 
+        ArrayList<Pair<String, String>> pathsWithPositions = new ArrayList<>();
+        Elements paths = this.file.body().getElementsByTag("path");
 
         for (Element path : paths) {
-            String temp = path.attribute("d").toString();
-            stringPaths.add(temp.substring(3,temp.length()-2));
+            String pathData = path.attr("d");
+            String xPos = "0";
+            String yPos = "0";
+            pathsWithPositions.add(new Pair<>(pathData, "translate(" + xPos + "," + yPos + ")"));
         }
-        return stringPaths;
+        return pathsWithPositions;
+    }
+
+    public Region createRegionFromSvg(String svgData, String position, Pane container) {
+        SVGPath svgPath = new SVGPath();
+        svgPath.setContent(svgData);
+        
+        Region svgRegion = new Region();
+        svgRegion.setShape(svgPath);
+
+        // .st0{fill:#ECECEC;stroke:#000000;stroke-width:0.2;stroke-linecap:round;stroke-linejoin:round;}x\
+
+        svgRegion.setStyle("-fx-background-color: white; -fx-border-color: black; -fx-transform: 0px 0px");
+        svgRegion.setPrefSize(container.getPrefWidth(), container.getPrefHeight()); // Imposta le dimensioni preferite
+    
+        // Parsing della trasformazione di posizione
+        double xPos = 0;
+        double yPos = 0;
+        if (position.startsWith("translate(") && position.endsWith(")")) {
+            String[] positionValues = position.substring(10, position.length() - 1).split(",");
+            if (positionValues.length == 2) {
+                xPos = Double.parseDouble(positionValues[0]);
+                yPos = Double.parseDouble(positionValues[1]);
+            }
+        }
+    
+        // Applicazione della traslazione
+        svgRegion.setLayoutX(xPos);
+        svgRegion.setLayoutY(yPos);
+    
+        return svgRegion;
     }
 }
--- src/main/java/com/assets/generalAssets/startPageController.java
+++ src/main/java/com/assets/generalAssets/startPageController.java
@@ -9,46 +9,39 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.shape.SVGPath;
+import javafx.util.Pair;
 
 
 public class startPageController {
     
     @FXML
     private Pane mapContainer;
-  
+    
+    
   
     @FXML
     private void initialize() {
-
         SVGParser svgParser = new SVGParser("src\\main\\resources\\com\\world.svg");
-        
-        ArrayList<String> svgPaths = svgParser.parseFile();
+        ArrayList<Pair<String, String>> svgPaths = svgParser.parseFile();
 
-        // crea arraylist di region e stampale come fai sotto
-        ArrayList<Region> regions = new ArrayList<Region>();
-        for (int i = 0; i < svgPaths.size(); i++){
-            regions.add(createRegionFromSvg(svgPaths.get(i)));
-            mapContainer.getChildren().add(regions.get(i)); 
+        double offsetX = 0; // Offset iniziale X nel contenitore
+        double offsetY = 0; // Offset iniziale Y nel contenitore
+
+        // Loop sui path dell'SVG
+        for (Pair<String, String> svgPathData : svgPaths) {
+            Region region = svgParser.createRegionFromSvg(svgPathData.getKey(), svgPathData.getValue(), mapContainer);
+            
+            // Imposta la posizione della regione considerando l'offset
+            region.setLayoutX(offsetX);
+            region.setLayoutY(offsetY);
+
+            mapContainer.getChildren().add(region); // Aggiungi la regione al contenitore
+
+            // qui potresti voler considerare la dimensione della regione appena aggiunta per calcolare l'offset successivo
+            
         }
     }
 
-
-    private static Region createRegionFromSvg(String svgData) {
-        SVGPath svgPath = new SVGPath();
-        svgPath.setContent(svgData);
-        
-        //DA QUA E' UNA MERDA
-        
-
-        Region svgRegion = new Region();
-        svgRegion.setShape(svgPath);
-        svgRegion.setStyle("-fx-background-color: white; -fx-border-color: black");
-        svgRegion.setPrefSize(100, 100); // Imposta le dimensioni preferite
-
-        return svgRegion;
-    }
-
-    // DA QUA VA BENE
 
     @FXML
     private void switchToSecondary() throws IOException {

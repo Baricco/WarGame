2be0b6b906ce195e54099ffe1fb8783a382031ea
LorenzoSomma <317154@studenti.unimore.it>
2024-04-08 15:43:42 +0200

    fxml messi e ronaldo

--- src/main/java/com/assets/generalAssets/startPageController.java
+++ src/main/java/com/assets/generalAssets/startPageController.java
@@ -12,13 +12,151 @@ import javafx.fxml.Initializable;
 
 
 public class startPageController implements Initializable{
-    
+
     @FXML
     private SVGPath AE;
 
-    
-    
+    @FXML
+    private SVGPath AF;
 
+    @FXML
+    private SVGPath AL;
+
+    @FXML
+    private SVGPath AM;
+
+    @FXML
+    private SVGPath AO;
+
+    @FXML
+    private SVGPath AR;
+
+    @FXML
+    private SVGPath AT;
+
+    @FXML
+    private SVGPath ATL;
+
+    @FXML
+    private SVGPath AU;
+
+    @FXML
+    private SVGPath BD;
+
+    @FXML
+    private SVGPath BE;
+
+    @FXML
+    private SVGPath BF;
+
+    @FXML
+    private SVGPath BI;
+
+    @FXML
+    private SVGPath BN;
+
+    @FXML
+    private SVGPath BO;
+
+    @FXML
+    private SVGPath BR;
+
+    @FXML
+    private SVGPath BS;
+
+    @FXML
+    private SVGPath BY;
+    
+    @FXML
+    private SVGPath CA;
+
+    @FXML
+    private SVGPath CF;
+
+    @FXML
+    private SVGPath CH;
+
+    @FXML
+    private SVGPath CN;
+
+    @FXML
+    private SVGPath CO;
+
+    @FXML
+    private SVGPath CY;
+
+    @FXML
+    private SVGPath DE;
+
+    @FXML
+    private SVGPath DZ;
+
+    @FXML
+    private SVGPath ER;
+
+    @FXML
+    private SVGPath ES;
+
+    @FXML
+    private SVGPath FI;
+
+    @FXML
+    private SVGPath FJ;
+
+    @FXML
+    private SVGPath GB;
+
+    @FXML
+    private SVGPath GL;
+
+    @FXML
+    private SVGPath GM;
+
+    @FXML
+    private SVGPath IL;
+
+    @FXML
+    private SVGPath JP;
+
+    @FXML
+    private SVGPath KG;
+
+    @FXML
+    private SVGPath KH;
+
+    @FXML
+    private SVGPath KP;
+
+    @FXML
+    private SVGPath LY;
+
+    @FXML
+    private SVGPath MD;
+
+    @FXML
+    private SVGPath MX;
+
+    @FXML
+    private SVGPath NZ;
+
+    @FXML
+    private SVGPath PL;
+
+    @FXML
+    private SVGPath TL;
+
+    @FXML
+    private SVGPath TR;
+
+    @FXML
+    private SVGPath TT;
+
+    @FXML
+    private SVGPath US;
+
+    @FXML
+    private SVGPath BZ;
+    
     @FXML
     private Pane mapContainer;    
  

3c2f45f883d31f33701a5291b2706eb7cd51b5cd
LorenzoSomma <317154@studenti.unimore.it>
2024-05-17 00:57:17 +0200

    ora la listview dei tuoi stati si aggiorna

--- src/main/java/com/assets/gameAssets/GameManager.java
+++ src/main/java/com/assets/gameAssets/GameManager.java
@@ -350,6 +350,7 @@ public class GameManager {
             System.out.println("Attacker Won");
 
             try { getHumanPlayer().occupyState(curSelectedState); } catch(Exception e) { e.printStackTrace(); }
+            try { addStringToListView("#playerStateConqueredTerritoriesListView", curSelectedState.getName());} catch(Exception e) { }
 
             // Si aggiornano le truppe perse
         }
@@ -617,7 +618,7 @@ public class GameManager {
         
         this.getHumanPlayer().setOriginalState(clickedState);
             
-        addStringToListView("#playerStateAlliedStatesListView", clickedState.getName());
+        addStringToListView("#playerStateConqueredTerritoriesListView", clickedState.getName());
 
         refreshPlayerMenuByState(clickedState.getId());
         refreshSideMenu(clickedState);

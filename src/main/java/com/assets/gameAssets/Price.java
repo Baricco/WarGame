7e90d7e4e4c3db01d000f9050ff66a89603ceefd
Baricco <filibarucci@gmail.com>
2024-05-31 14:40:38 +0200

    ho sistemato Price.java

--- src/main/java/com/assets/gameAssets/Price.java
+++ src/main/java/com/assets/gameAssets/Price.java
@@ -1,15 +1,34 @@
 package com.assets.gameAssets;
 
 public class Price {
-    
+
+
+    // Attack Prices
     public static final int LAND_ATTACK_PRICE_PER_DICE = 5000;
     public static final int SEA_ATTACK_PRICE_PER_DICE = 20000;
+
+    // Recruit Prices
     public static final int RECRUIT_PRICE_PER_DICE = 40000;
+    
+    // Citizen Work Prices
+    public static final int GOVERNMENT_INCENTIVES_PRICE = -1;
+    public static final int BUILDINGS_RENOVATION_PRICE = -1;
+    public static final int BUILDINGS_CONSTRUCTION_PRICE = -1;
+    public static final int HARVESTING_IMPROVEMENT_TURN_PRICE = -1;
+    public static final int INDUSTRIAL_IMPROVEMENT_TURN_PRICE = -1;
     public static final int SMALL_CITIZEN_WORK_PRICE = 300000;
     public static final int GREAT_CITIZEN_WORK_PRICE = 800000;
+    
+    // Fortification Prices
     public static final int FORTIFICATION_PRICE_PER_LEVEL = 600000;
+    
+    // Supply Prices
     public static final int SUPPLY_PRICE_PER_TERRITORY = 3000;
+    
+    // Negotiation Prices
     public static final int NEGOTIATION_PRICE = 15000;
+
+
 
 private Price() {}
 

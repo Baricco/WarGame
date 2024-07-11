9a817f3403e0416eb89bba3fa132f3bd37bc5afb
Baricco <filibarucci@gmail.com>
2024-07-11 17:12:46 +0200

    Revert "risposta i Bicocchi"
    This reverts commit fd741aafa67f878ba753a87826097c6bb9226490.

--- src/main/java/com/assets/gameAssets/GameManager.java
+++ src/main/java/com/assets/gameAssets/GameManager.java
@@ -951,8 +951,6 @@ public class GameManager {
         int faceNumber = 0;
         for (Node curImageView : diceContainer.getChildren()) {
             
-            // le immagini devono trovarsi in un percorso uguale a quello di gamemanager da java in poi e poi vanno prese con getClass.getResource(nomeFile)
-
             ((ImageView)curImageView).setImage(new Image("src/main/resources/com/icons/dices/" + dirNames[i] + "/" +  dirNames[i] + "_" + faceNumber + ".png"));
             faceNumber++;
         }

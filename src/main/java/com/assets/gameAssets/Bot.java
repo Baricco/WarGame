32b6099c75977cd85d070f5e1cb5c98f91abcec9
LorenzoSomma <317154@studenti.unimore.it>
2024-07-14 17:00:59 +0200

    ho fatto le immagini

--- src/main/java/com/assets/gameAssets/Bot.java
+++ src/main/java/com/assets/gameAssets/Bot.java
@@ -20,7 +20,11 @@ public class Bot extends Player implements Runnable {
         "Vedi te se mi devo fa dì male dal figlio di Totò Riina",
         "Meglio fare la cacca nella doccia che fare la doccia nella cacca",
         "I <3 Dittatura del Proletariato",
-        "Meglio un giorno in pretura che vent'anni di dittatura"
+        "Meglio un giorno in pretura che vent'anni di dittatura",
+        "Madonna cara",
+        "Baricco spero ti venga un colpo",
+        "Sborricchio",
+        "Galassia lontana, tua madre puttana"
     };
 
     private static RandomGenerator rnd = RandomGenerator.getDefault();

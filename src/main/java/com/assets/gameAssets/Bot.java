e1b3b9a1ad4cc9564cc21f324ac3be9d1960d2d0
Baricco <filibarucci@gmail.com>
2024-07-18 17:40:09 +0200

    Ho impostato i Bot

--- src/main/java/com/assets/gameAssets/Bot.java
+++ src/main/java/com/assets/gameAssets/Bot.java
@@ -6,6 +6,9 @@ import com.assets.generalAssets.App;
 
 public class Bot extends Player implements Runnable {
     
+    final static int MAX_ACTIONS = 5;
+
+    /*
     private String[] stringozze = { 
         "Sei stato adottato",
         "Tuo padre è un tossico",
@@ -26,6 +29,7 @@ public class Bot extends Player implements Runnable {
         "Sborricchio",
         "Galassia lontana, tua madre puttana"
     };
+    */
 
     private static RandomGenerator rnd = RandomGenerator.getDefault();
 
@@ -42,7 +46,49 @@ public class Bot extends Player implements Runnable {
 
         // TODO: Qui ci vanno tutte le cose che fa il bot quando gioca
 
-        System.out.println(this.getName() + " says: " + stringozze[rnd.nextInt(stringozze.length)]);
+        // System.out.println(this.getName() + " says: " + stringozze[rnd.nextInt(stringozze.length)]);
+        
+        int actionNumber = rnd.nextInt(MAX_ACTIONS);
+
+        for (int i = 0; i < actionNumber; i++) {
+
+            int curAction = rnd.nextInt(6); // QUI COME NUMERO BISOGNA METTERE IL NUMERO DI AZIONI CHE SI POSSONO FARE
+
+            switch (curAction) {
+                case 0:     // Attacco
+                    
+                    break;
+
+                case 1:     // Negoziazione
+                    
+                    break;
+
+                case 2:     // Opere per i Cittadini
+                    
+                    break;
+
+                case 3:     // Fortificazione
+
+                    break;
+                
+                case 4:     // Recluta
+
+                    break;
+
+                case 5:     // Rifornimento
+                    
+                    break;
+
+                default:    // Se entra qui dentro abbiamo un problema
+                    
+                    break;
+            }
+
+
+            
+
+            try { Thread.sleep(rnd.nextLong(500, 2000)); } catch (InterruptedException e) { return; }
+        }
 
         try { Thread.sleep(rnd.nextLong(500, 2000)); } catch (InterruptedException e) { return; }
 
